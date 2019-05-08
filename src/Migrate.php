267234cbaf6e036b4b1fb3ce2fa5df0e8384dc58
Fernando Miranda <fndmiranda@gmail.com>
2019-05-08 19:27:29 -0300

    Start create migrate status

--- src/Migrate.php
+++ src/Migrate.php
@@ -2,10 +2,21 @@
 
 namespace Fndmiranda\DataMigrate;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+
 class Migrate
 {
-    public function migrate()
+    /**
+     * @param Model|string $model
+     * @param array|Collection $data
+     * @param array|Collection $options
+     */
+    public function status($model, $data, $options = [])
     {
-        
+        $data = $data instanceof Collection ? $data : Collection::make($data);
+        $options = $options instanceof Collection ? $options : Collection::make($options);
+
+        dump($options);
     }
 }
