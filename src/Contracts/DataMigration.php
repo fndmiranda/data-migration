930fac69767e5edab3361c91e7c376cdbff10aa5
Fernando Miranda <fndmiranda@gmail.com>
2019-05-16 23:44:05 -0300

    Fix name package

--- src/Contracts/DataMigration.php
+++ src/Contracts/DataMigration.php
@@ -8,21 +8,21 @@ use Illuminate\Support\Collection;
 interface DataMigration
 {
     /**
-     * Get the model being used by the data-migrate.
+     * Get the model being used by the data-migration.
      *
      * @return string|Model
      */
     public function model();
 
     /**
-     * Get the data being used by the data-migrate.
+     * Get the data being used by the data-migration.
      *
      * @return array|Collection
      */
     public function data();
 
     /**
-     * Get the data options being used by the data-migrate.
+     * Get the data options being used by the data-migration.
      *
      * @return array|Collection
      */
